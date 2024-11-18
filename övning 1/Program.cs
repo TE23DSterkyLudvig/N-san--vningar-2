8f2b5c0e8a308586371fba21ec1b845963e8cca1
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-11-18 09:10:19 +0100

    klar

---
+++ övning 1/Program.cs
@@ -0,0 +1,12 @@
+﻿string[]Leksaker = ["tåg", "bil", "mus", "råtta", "falafel"];
+
+for (int i = 0; i < 1; i++)
+{
+   Console.WriteLine(Leksaker[0]);
+   Console.WriteLine(Leksaker[1]);
+   Console.WriteLine(Leksaker[2]);
+   Console.WriteLine(Leksaker[3]);
+   Console.WriteLine(Leksaker[4]);
+}
+
+Console.ReadLine();
