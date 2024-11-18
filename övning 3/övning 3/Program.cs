db223ae3f2139ced5dee7e70ababe82869888295
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-11-18 09:48:27 +0100

    klar

---
+++ övning 3/övning 3/Program.cs
@@ -0,0 +1,21 @@
+﻿List<string> cities = new List<string>();
+string svar = "";
+
+while(true){
+System.Console.WriteLine("Skriv ett namn på en stad eller skriv exit för att avsluta programmet");
+svar = Console.ReadLine();
+cities.Add(svar);
+
+if(svar.ToLower() == "exit"){
+    cities.Remove("exit");
+    break;
+}
+}
+
+foreach(string city in cities)
+{
+    Console.WriteLine(city);
+    Thread.Sleep(1000);
+}
+Console.ReadLine();
+
